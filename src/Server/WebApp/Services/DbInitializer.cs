b418941f9235331cac285edeb246619bbdeb7174
John Pankowicz <johnpankowicz@hotmail.com>
2017-10-20 20:49:21 -0400

    Added security policies for Administrator & Editor and addded checks in Admin, Addtags and Fixasr controllers. Changed Todo(gm) to Todo-g for tasks.

--- src/Server/WebApp/Services/DbInitializer.cs
+++ src/Server/WebApp/Services/DbInitializer.cs
@@ -2,9 +2,11 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using WebApp.Data;
 using WebApp.Models;
@@ -15,10 +17,11 @@ namespace WebApp.Services
     // https://stackoverflow.com/questions/40027388/cannot-get-the-usermanager-class/40046290#40046290
     public static class DbInitializer
     {
-        static Data.ApplicationDbContext context;
-        static UserManager<Models.ApplicationUser> userManager;
+        static ApplicationDbContext context;
+        static UserManager<ApplicationUser> userManager;
         static RoleManager<IdentityRole> roleManager;
-        public static async Task Initialize(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        public static async Task Initialize(ApplicationDbContext context, UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager, IConfiguration configuration)
         //public static async Task Initialize(IApplicationBuilder app)
         {
             // Get the db context of the Application
@@ -34,28 +37,61 @@ namespace WebApp.Services
             // Ensure that the database exists and all pending migrations are applied.
             context.Database.Migrate();
 
-            // Create roles
-            string[] roles = new string[] { "UserManager", "StaffManager" };
-            foreach (string role in roles)
-            {
-                if (!await roleManager.RoleExistsAsync(role))
-                {
-                    await roleManager.CreateAsync(new IdentityRole(role));
-                }
-            }
+            // The following was from before we switch to using claims.
+            //// Create roles. This adds them to the table dbo.AspNetRoles.
+            //string[] roles = new string[] { "UserManager", "StaffManager" };
+            //foreach (string role in roles)
+            //{
+            //    if (!await roleManager.RoleExistsAsync(role))
+            //    {
+            //        await roleManager.CreateAsync(new IdentityRole(role));
+            //    }
+            //}
 
-            // Create admin user
+            // Create admin user. This adds it to the table dbo.AspNetUsers
+            string admin_username = configuration["AdminUser:Username"];
+            string admin_password = configuration["AdminUser:Password"];
+            string admin_email = configuration["AdminUser:Email"];
             if (!context.Users.Any())
             {
-                await userManager.CreateAsync(new ApplicationUser() { UserName = "info@example.com", Email = "info@example.com" }, "p@ssw0rd");
+                await userManager.CreateAsync(new ApplicationUser() { UserName = admin_username, Email = admin_email }, admin_password);
             }
 
-            // Ensure admin privileges
-            ApplicationUser admin = await userManager.FindByEmailAsync("info@example.com");
-            foreach (string role in roles)
+            // Confirm the admin's email
+            ApplicationUser admin_user;
+            admin_user = await userManager.FindByEmailAsync(admin_email);
+            var emailConfirmationCode = await userManager.GenerateEmailConfirmationTokenAsync(admin_user);
+            var confirmResult = await userManager.ConfirmEmailAsync(admin_user, emailConfirmationCode);
+
+            // The following was from before we switch to using claims.
+            //// assign admin privileges. This adds and enty in table dbo.AspNetUserRoles
+            //ApplicationUser admin = await userManager.FindByEmailAsync("info@example.com");
+            //foreach (string role in roles)
+            //{
+            //    await userManager.AddToRoleAsync(admin, role);
+            //}
+
+
+            // assign admin privileges. This adds and enty in table AspNetUserClaims
+            var claims = await userManager.GetClaimsAsync(admin_user);
+            var jobClaim = claims.FirstOrDefault(c => c.Type == "role");
+            if (jobClaim == null)
             {
-                await userManager.AddToRoleAsync(admin, role);
+                await userManager.AddClaimAsync(admin_user, new Claim("role", "Administrator"));
             }
         }
     }
 }
+
+/*                                                SUMMARY
+ *                   CALL                                    CREATES ENTRY IN TABLE
+ * roleManager.CreateAsync(new IdentityRole( ... ))          dbo.AspNetRoles
+ * userManager.CreateAsync(new ApplicationUser() { ... }     dbo.AspNetUsers
+ * userManager.AddToRoleAsync( ... )                         dbo.AspNetUserRoles
+ * userManager.AddClaimAsync(user, new Claim( ... )          dbo.AspNetUserClaims
+ * 
+ *      OTHER IDENTITY TABLES
+ *      dbo.AspNetRoleClaims
+ *      dbo.AspNetUserLogins
+ *      dbo.AspNetUserTokens
+ * */
--- src/Server/WebApp/Startup.cs
+++ src/Server/WebApp/Startup.cs
@@ -32,59 +32,9 @@ namespace WebApp
         }
         public IConfiguration Configuration { get; set; }
 
-        //public Startup(IHostingEnvironment env)
-        //{
-        //    //Console.WriteLine("Environment = " + env.EnvironmentName);
-
-        //    // Set up configuration sources.
-        //    var builder = new ConfigurationBuilder()
-
-        //        // JP: ### Conversion to ASP.NET Core ###
-        //        // These changes were made to new template.
-        //        .SetBasePath(env.ContentRootPath)
-        //        //.AddJsonFile("appsettings.json")
-        //        .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
-        //        .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
-        //        // appsettings.production.json is stored outside of the source code repo since it
-        //        // contains user secrets. It is copied during publishing to the publish folder. 
-        //        // See build-publish.sh in Solution items.
-
-        //    // Explanation of Asp.Net Core configuration and how to flow it to other parts of the app:
-        //    // https://docs.microsoft.com/en-us/aspnet/core/fundamentals/configuration
-
-        //    if (env.IsDevelopment())
-        //    {
-        //        builder.AddUserSecrets();
-        //        // We use the User Secrets store for secrets we don't want to check-in to Github during development.
-        //        // During production, we get these secrets from the appsettings.production.json file.
-        //        // Using the secret store:
-        //        // Open a command prompt at the project root folder and use these commands:
-        //        //     dotnet user-secrets--help
-        //        //     dotnet user-secrets set MySecret ValueOfMySecret
-        //        //     dotnet user-secrets list
-        //        // For example, to set the Google+ ClientSecret to "xxxxxxxx", do:
-        //        //     dotnet user-secrets set ExternalAuth:Google:ClientSecret xxxxxxxx
-        //        // For more details on using the user secret store see:
-        //        //   http://go.microsoft.com/fwlink/?LinkID=532709
-        //        //   http://asp.net-hacker.rocks/2016/07/11/user-secrets-in-aspnetcore.html
-        //    }
-
-        //    builder.AddEnvironmentVariables();
-        //    Configuration = builder.Build();
-
-        //    System.Console.WriteLine("ConnectionStrng = " + Configuration["Data:DefaultConnection:ConnectionString"]);
-        //    System.Console.WriteLine("ClientId = " + Configuration["ExternalAuth:Google:ClientId"]);
-        //    System.Console.WriteLine("DatafilesPath = " + Configuration["Datafiles:DatafilesPath"]);
-        //}
-
-        //public IConfigurationRoot Configuration { get; set; }
-
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
-
-
             // Here we will create a service for handling the configuration settings that were stored in
             // "Configuration" in the Startup() method. This has two advantages:
             // 1. We will be able to access the configuration settings from controllers using Dependency Injection.
@@ -105,18 +55,11 @@ namespace WebApp
                 Console.WriteLine("Datafile path = " + myOptions.DatafilesPath);
             });
 
-
-            // JP: ### Conversion to ASP.NET Core ###
-            // JP: The latest template for ASP.NET Core removes the calls to AddEntityFramework and AddSqlServer.
-            // Asp.Net Core documentation: https://docs.microsoft.com/en-us/aspnet/core/
-            //services.AddEntityFramework()
-            //    .AddSqlServer()
             services.AddDbContext<ApplicationDbContext>(options =>
                 options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
             // In development, we get this value from appsettings.json. In production, the value
             // in appsettings.production.json overrides this value.
 
-            // This was added in upgrade to .Net SDK 2.0
             services.AddAuthentication()
             .AddGoogle(options => {
                 options.ClientId = Configuration["ExternalAuth:Google:ClientId"];
@@ -126,8 +69,7 @@ namespace WebApp
             services.AddIdentity<ApplicationUser, IdentityRole>(options =>
             {
                 // Govmeeting: Set options for cookie expiration.
-
-                // Todo (gm) - While upgrading to .NET SDK 2.0, I was getting an error on the next two line so
+                // Todo-g - While upgrading to .NET SDK 2.0, I was getting an error on the next two line so
                 // I commented them out. Error = "IdentityOptions does not contain a definition for Cookies"
                 //options.Cookies.ApplicationCookie.SlidingExpiration = true;
                 //options.Cookies.ApplicationCookie.ExpireTimeSpan = TimeSpan.FromHours(1);
@@ -138,7 +80,7 @@ namespace WebApp
                 options.Lockout.MaxFailedAccessAttempts = 5;
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5); // amount of time they are locked out
                 options.Lockout.AllowedForNewUsers = true;
-                // Todo(gm): We should send the admin an email if someone is locked out.
+                // Todo-g We should send the admin an email if someone is locked out.
                 // Govmeeting: Require email confirmation
                 options.SignIn.RequireConfirmedEmail = true;
             })
@@ -156,20 +98,30 @@ namespace WebApp
             // https://docs.asp.net/en/latest/security/authorization/claims.html
             services.AddAuthorization(options =>
             {
-                options.AddPolicy("SysAdmin", policy =>
+                options.AddPolicy("Administrator", policy =>
                 {
-                    policy.RequireClaim("Administrator", "System");
+                    policy.RequireClaim("role", "administrator");
                 });
 
-                // Todo(gm) We need policies that are more dynamic and flexible.
-                // These will work temporarily.
+                options.AddPolicy("Editor", policy =>
+                {
+                    policy.RequireClaim("role", "editor");
+                });
+
+                options.AddPolicy("Proofreader", policy =>
+                {
+                    policy.RequireClaim("role", "proofreader");
+                });
+
                 options.AddPolicy("PhillyEditor", policy =>
                 {
-                    policy.RequireClaim("Editor", "Philadelphia");
+                    policy.RequireClaim("role", "editor");
+                    policy.RequireClaim("location", "Philadelphia");
                 });
                 options.AddPolicy("BbhEditor", policy =>
                 {
-                    policy.RequireClaim("Editor", "Boothbay Harbor");
+                    policy.RequireClaim("role", "editor");
+                    policy.RequireClaim("location", "Boothbay Harbor");
                 });
 
                 //options.AddPolicy("DevInterns", policy =>
@@ -237,7 +189,7 @@ namespace WebApp
                 // Using migrations with asp.net core: https://dzone.com/articles/how-to-use-migration-with-entity-framework-core 
                 try
                 {
-                    // Todo(gm) - We should be able to replace all the code below with:
+                    // Todo-g - We should be able to replace all the code below with:
                     //          db.Database.Migrate();
                     // if we add another argument to the Configure() argument list:
                     //          public void Configure( ...... , ApplicationDbContext db)
@@ -280,22 +232,6 @@ namespace WebApp
 
             app.UseAuthentication();
 
-            // Upgrading to .Net SDK 2.0 - this configurtion is now in ConfigureServices method
-            //// To configure external authentication please see http://go.microsoft.com/fwlink/?LinkID=532715
-            //// Govmeeting: Add Google middleware authentication. We also added 
-            //// "Microsoft.AspNet.Authentication.Google" dependency in project.json and using statement above.
-            //// http://localhost:60366/signin-google
-            //app.UseGoogleAuthentication(new GoogleOptions
-            //{
-            //    ClientId = Configuration["ExternalAuth:Google:ClientId"],
-            //    ClientSecret = Configuration["ExternalAuth:Google:ClientSecret"],
-            //    AuthenticationScheme = "Google",
-
-                //// JP: ### Conversion to ASP.NET Core ###
-                ////SignInScheme = new Microsoft.AspNet.Identity.IdentityCookieOptions().ExternalCookieAuthenticationScheme
-                //SignInScheme = new Microsoft.AspNetCore.Identity.IdentityCookieOptions().ExternalCookieAuthenticationScheme;
-            //});
-
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
@@ -317,12 +253,8 @@ namespace WebApp
             });
 
             // Create seed data
-            DbInitializer.Initialize(context, userManager, roleManager).Wait();
+            DbInitializer.Initialize(context, userManager, roleManager, Configuration).Wait();
             //DbInitializer.Initialize(app).Wait();
         }
-
-        // This was the entry point for a normal Asp.Net application. But with Asp.Net Core, the
-        // entry point is now in program.cs.
-        //   public static void Main(string[] args) => WebApplication.Run<Startup>(args);
     }
 }
