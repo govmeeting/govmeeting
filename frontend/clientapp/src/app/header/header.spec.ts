d53a16f65686010aa5907be77ddcf611a28d04ed
Jonathan Parker <jon@roadhouse.com.au>
2020-08-14 19:39:25 +1000

    Fixed header tests

--- frontend/clientapp/src/app/header/header.spec.ts
+++ frontend/clientapp/src/app/header/header.spec.ts
@@ -1,6 +1,12 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HeaderComponent } from './header';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { NavService } from '../sidenav/nav.service';
+
+class MockNavService {
+  openNav() {}
+};
 
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -8,7 +14,9 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HeaderComponent ]
+      declarations: [ HeaderComponent ],
+      providers: [{ provide: NavService, useClass: MockNavService }],
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
   }));
