be2059e7f3a392fdb09f677f28404963820fb872
John Pankowicz <johnpankowicz@hotmail.com>
2023-03-16 14:02:45 -0400

    Generate client side code with openapi-generate
    Weird function names: apiWeatherForecastGetGet

---
+++ src/WebUI/WebApp/clientapp/src/app/core/api/v1/api.module.ts
@@ -0,0 +1,37 @@
+import { NgModule, ModuleWithProviders, SkipSelf, Optional } from '@angular/core';
+import { Configuration } from './configuration';
+import { HttpClient } from '@angular/common/http';
+
+import { EditMeetingService } from './api/editMeeting.service';
+import { GovLocationService } from './api/govLocation.service';
+import { GovbodyService } from './api/govbody.service';
+import { HealthCheckService } from './api/healthCheck.service';
+import { VideoService } from './api/video.service';
+import { ViewMeetingService } from './api/viewMeeting.service';
+import { WeatherForecastService } from './api/weatherForecast.service';
+
+@NgModule({
+  imports:      [],
+  declarations: [],
+  exports:      [],
+  providers: []
+})
+export class ApiModule {
+    public static forRoot(configurationFactory: () => Configuration): ModuleWithProviders<ApiModule> {
+        return {
+            ngModule: ApiModule,
+            providers: [ { provide: Configuration, useFactory: configurationFactory } ]
+        };
+    }
+
+    constructor( @Optional() @SkipSelf() parentModule: ApiModule,
+                 @Optional() http: HttpClient) {
+        if (parentModule) {
+            throw new Error('ApiModule is already loaded. Import in your base AppModule only.');
+        }
+        if (!http) {
+            throw new Error('You need to import the HttpClientModule in your AppModule! \n' +
+            'See also https://github.com/angular/angular/issues/20575');
+        }
+    }
+}
