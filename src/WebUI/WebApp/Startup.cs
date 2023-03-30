57c6b6c595ad652b15bbdf91b931bd61cc6a612d
John Pankowicz <johnpankowicz@hotmail.com>
2023-03-30 11:29:53 -0400

    Create new webapp. rename prior to webapp_PRIOR

--- src/WebUI/WebApp/Startup.cs
+++ src/WebUI/WebApp/Startup.cs
@@ -1,295 +1,51 @@
-using GM.Application.AppCore.Common;
-using GM.Application.AppCore.Interfaces;
-using GM.Application.Configuration;
-using GM.Infrastructure.FileDataRepositories.EditMeetings;
-using GM.Infrastructure.FileDataRepositories.ViewMeetings;
-using GM.Infrastructure.InfraCore.Data;
-using GM.Utilities;
-using GM.WebUI.WebApp.Services;
-using GM.WebUI.WebApp.StartupCustomizations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.DataProtection;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc.Razor;
-using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.OpenApi.Models;
-using NLog;
-using System.IO;
-using System.Reflection;
+using Microsoft.Extensions.Logging;
 
-namespace GM.WebUI.WebApp
+namespace webapp
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration, IHostEnvironment environment)
+        public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
-            Environment = environment;
         }
 
-        // NLog will set logger.Name to "GM.WebUI.WebApp.Startup"
-        // This is the logger "name" that we can refer to in NLog.config
-        NLog.Logger logger;
         public IConfiguration Configuration { get; }
-        public IHostEnvironment Environment { get; }
 
-        public void ConfigureDockerServices(IServiceCollection services)
-        {
-            services.AddDataProtection()
-                .SetApplicationName("govmeeting")
-                .PersistKeysToFileSystem(new DirectoryInfo(@"./"));
-            ConfigureServices(services);
-        }
-
+        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            ConfigureLoggingService();
-            logger.Info("Configure Logging Service");
-            logger.Info($"ENV={Environment.EnvironmentName}");
-
-            ConfigureAppsettings(services);
-            logger.Info("Configure Appsettings");
-
-            // We can choose to develop with a Postgres DB instead of the default IisExpress.
-            // Note that if you switch, you need delete the Migrations folder in Infra_Core_Lib
-            // and run "dotnet ef migrations --project ../../InfraCore_Lib Initial" from the WebApp folder.
-            string dbType = Configuration["AppSettings:DatabaseType"];
-            string connectionString = (dbType == "postgres") ?
-                Configuration["AppSettings:PgConnectionString"] :
-                Configuration["AppSettings:ConnectionString"];
-
-            ConfigureDatabaseServices.Configure(services, Environment.EnvironmentName,
-                dbType, connectionString);
-            logger.Info($"Configure Database. Connection={connectionString}");
-
-            // Hard code postgress - replace above code for degugging startup issues
-            //string connectionString = "Host=localhost;Port=5432;Username=postgres;Password=Adm1nPa33;Database=Govmeeting2";
-            //ConfigureDatabaseServices.Configure(services, Environment.EnvironmentName, "postgres", connectionString);
-
-            //services.AddHealthChecks();
-            //logger.Info("AddHealthChecks");
-
-            //AUTH// StartupAuth.ConfigureIdentity(services, Configuration, logger);
-            // logger.Info("ConfigureIdentity");
-
-            services.AddControllersWithViews();
-            logger.Info("Add services for Web API, MVC & Razor Views");
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Govmeeting.api", Version = "v1" });
-            });
-
-            logger.Info("Add services for Swagger Document");
-
-            services.AddRazorPages();
-            logger.Info("Add services for Razor Pages");
-
-            services.Configure<RazorViewEngineOptions>(options => options.ViewLocationExpanders.Add(new FeatureLocationExpander()));
-            logger.Info("Enable Feature Folders");
-            // https://scottsauber.com/2016/04/25/feature-folder-structure-in-asp-net-core/
-
-            // Angular files will be served from this directory in production. 
-            services.AddSpaStaticFiles(configuration => configuration.RootPath = "clientapp/dist");
-            logger.Info("AddSpaStaticFiles");
-
-            // get the current user for auditing
-            services.AddScoped<ICurrentUserService, CurrentUserService>();
-
-            Assembly executingAssembly = Assembly.GetExecutingAssembly();
-            services.AddCQR(executingAssembly);
-            logger.Info("Configure CQR Services");
-
-            services.AddTransient<IEmailSender, AuthMessageSender>();
-            services.AddTransient<ISmsSender, AuthMessageSender>();
-            logger.Info("Add email and sms");
-
-            services.AddScoped<ValidateReCaptchaAttribute>();
-            logger.Info("Add ValidateReCaptchaAttribute");
-
-            services.AddTransient<ISeedDatabase, SeedDatabase>();
-            services.AddTransient<IViewMeetingRepository, ViewMeetingRepository>();
-            services.AddTransient<IEditMeetingRepository, EditMeetingRepository>();
-            logger.Info("Add SeedDatabase, ViewMeetingRepository, EditMeetingRepository");
+            services.AddControllers();
         }
 
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ISeedDatabase seedDatabase)
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            logger.Info("Entering startup -> Configure");
-            /*  NOTE: 
-             *  For Visual Studio, the URL's used during development are in
-             *      (WebApp) Project -> Properties -> Debug. Currently:
-             *      "App URL" = "http:/localhost:36029"
-             *      "Enable SSL" is checked. "https:/localhost:44333/"
-             *  For "dotnet run", the dev URL's are in WebApp\Properties\launchSettings.json,
-             *    Currently for IISExpress:
-             *      "applicationUrl": "http://localhost:36029",
-             *      "sslPort": 44333
-             *  For VsCode, the dev URL's are in clientapp/.vscode/launch.json. Currently:
-             *      "url": "http:/localhost:4200"
-             */
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
-                app.UseDatabaseErrorPage();
-                logger.Info("Is Development - use dev exception page & database error page");
-            }
-            else
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-                logger.Info("Is not Development - use exception handler & HSTS");
             }
 
             app.UseHttpsRedirection();
-            logger.Info("UseHttpsRedirection");
-
-            app.UseStaticFiles();
-            logger.Info("UseStaticFiles");
-
-            if (!env.IsDevelopment())
-            {
-                app.UseSpaStaticFiles();
-                logger.Info("If not dev, UseSpaStaticFiles");
-            }
 
             app.UseRouting();
-            logger.Info("UseRouting");
-
-            // START OF ENDPOINT ZONE
-
-            app.UseAuthentication();
-            logger.Info("UseAuthentication");
 
             app.UseAuthorization();
-            logger.Info("UseAuthorization");
-
-            DebugCurrentEndpoint(app);
-            logger.Info("DebugCurrentEndpoint");
-
-            // END OF ENDPOINT ZONE
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapControllerRoute(
-                    name: "default",
-                    pattern: "{controller}/{action=Index}/{id?}"
-                );
-                logger.Info("endpoints.MapControllerRoute");
-
-                endpoints.MapRazorPages();
-                logger.Info("endpoints.MapRazorPages");
-                //endpoints.MapHealthChecks("/health").RequireAuthorization();
-            });
-
-            if (env.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-                logger.Info("UseSwagger & UseSwaggerUI");
-            }
-
-            app.UseSpa(spa =>
-            {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
-                spa.Options.SourcePath = "clientapp";
-                logger.Info("spa.Options.SourcePath");
-
-                if (env.IsDevelopment())
-                {
-                    // spa.UseAngularCliServer(npmScript: "start");
-                    spa.UseProxyToSpaDevelopmentServer("http://localhost:4200");
-                    logger.Info("Is dev, spa.UseProxyToSpaDevelopmentServer");
-                }
-            });
-
-            //seedDatabase.Seed();
-
-        }
-
-        private void ConfigureLoggingService()
-        {
-            // Set a variable in the gdc which is be used in NLog.config for the
-            // base path of our app: ${gdc:item=appbasepath} 
-            string logfilesPath = GetFullPathOnEitherDevOrProdSystem(Configuration["Logging:LogfilesPath"]);
-            //string logfilesPath = GMFileAccess.GetFullPath(Configuration["AppSettings:LogfilesPath"]);
-            GlobalDiagnosticsContext.Set("logfilesPath", logfilesPath);
-
-            // Create an instance of NLog.Logger manually here since it is not available
-            // from dependency injection yet.
-            logger = LogManager.LoadConfiguration("nlog.config").GetCurrentClassLogger();
-        }
-
-
-        //public IServiceCollection AddCQR(this IServiceCollection services)
-        //{
-        //    services.AddMediatR(Assembly.GetExecutingAssembly());
-        //    services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
-        //    services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-        //    return services;
-        //}
-
-        private void ConfigureAppsettings(IServiceCollection services)
-        {
-            services.AddOptions();
-            services.Configure<AppSettings>(Configuration.GetSection("AppSettings"));
-            services.Configure<AppSettings>(myOptions =>
-            {
-                logger.Info("Modify the configuration path options to be full paths.");
-                // Modify the configuration path options to be full paths.
-                myOptions.DatafilesPath = GetFullPathOnEitherDevOrProdSystem(myOptions.DatafilesPath);
-                myOptions.TestdataPath = GetFullPathOnEitherDevOrProdSystem(myOptions.TestdataPath);
-                logger.Info("DatafilesPath: {0}, TestdataPath: {2}",
-                    myOptions.DatafilesPath, myOptions.TestdataPath);
+                endpoints.MapControllers();
             });
         }
-
-
-        // Examine contents of endpoint of current request (for debugging)
-        private void DebugCurrentEndpoint(IApplicationBuilder app)
-        {
-            app.Use(next => context =>
-            {
-                var endpoint = context.GetEndpoint();
-                if (endpoint == null)
-                {
-                    return next(context);
-                }
-
-                var route = (endpoint is RouteEndpoint routeEndpoint) ? routeEndpoint.RoutePattern : null;
-                var metadata = endpoint.Metadata;
-
-                return next(context);
-            });
-        }
-
-        /* GetFullPathOnEitherDevOrProdSystem is for creating/finding sibling folders to the project.
-        * These include: TESTDATA, DATAFILES, SECRETS.
-        * These folders must be outside the project folder so that they are not 
-        * included in the code repository.
-        * The names come from appsettings.json. In Development, this name is a
-        * sibling of the solution folder. But in production, it will normally be a rooted path.
-        * In production, we just return the path.
-        */
-        public string GetFullPathOnEitherDevOrProdSystem(string folder)
-        {
-            if (Environment.IsDevelopment())
-            {
-                return GMFileAccess.GetSolutionSiblingFolder(folder);
-            } else
-            // if (Path.IsPathRooted(folder))  // originally it was, if the path was rooted, it was production.
-            {
-                return folder;
-            }
-        }
-
     }
 }
