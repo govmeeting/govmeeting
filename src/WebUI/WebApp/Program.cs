6d68d5593e727af767b1d3f7f886adc201bd1b5b
John Pankowicz <johnpankowicz@hotmail.com>
2023-04-11 14:41:16 -0400

    Use Startup.cs in our .net 6 app
    We can still use Startup.cs when using .Net 6.
      This has some advantages. See:
      https://learn.microsoft.com/en-us/aspnet/core/migration/50-to-60
      ?view=aspnetcore-7.0&tabs=visual-studio#am6
    Also a first attempt to modify tasks.json & launch.json
      to build and debug clientapp independently from webapp.
    Add Directory.Build.props file to specify C# version

--- src/WebUI/WebApp/Program.cs
+++ src/WebUI/WebApp/Program.cs
@@ -1,47 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var startup = new Startup(builder.Configuration);
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// builder.Services.AddCors();
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(
-        builder =>
-        {
-            builder.WithOrigins("http://localhost:4200")
-                                .AllowAnyHeader()
-                                .AllowAnyMethod();
-        });
-});
-// "https://localhost:7103", 
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+        app.MapControllers();
 
-app.UseHttpsRedirection();
-
-// app.UseCors(builder =>
-// {
-//     builder
-//     .AllowAnyOrigin()
-//     .AllowAnyMethod()
-//     .AllowAnyHeader();
-// });
-app.UseCors();
-
-app.UseAuthorization();
-
-app.MapControllers();
+startup.Configure(app, app.Environment);
 
 app.Run();
