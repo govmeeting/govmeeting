2c172c1af1cd01e48de1f009c0486ee15821d1d7
John Pankowicz <johnpankowicz@hotmail.com>
2023-05-12 22:48:29 -0400

    Switch back to using prior webapp
    Rename current work on webapp to "webapp_new".
    Rename webapp_prior to "webapp".
    Explanation:
    Back in commit 79277a36, on 2023-03-30, we renamed current webapp
      "webapp_prior", created a new webapp from scratch and started
       adding the code that we want from webapp_prior.
    It may be easier to use the prior webapp as our starting point
     and change that to remove what we don't want.
    
    remove webapp_prior

--- src/WebUI/WebApp/Program.cs
+++ src/WebUI/WebApp/Program.cs
@@ -1,13 +1,68 @@
-var builder = WebApplication.CreateBuilder(args);
+using GM.Application.Configuration;
+using GM.Infrastructure.InfraCore.Data;
+using GM.WebUI.WebApp.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using NLog.Web;
+using System;
 
-var startup = new Startup(builder.Configuration);
+namespace GM.WebUI.WebApp
+{
+    public class Program
+    {
+        static IWebHostEnvironment _Env;
 
-startup.ConfigureServices(builder.Services);
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("=====================In Main====================");
+            var host = CreateHostBuilder(args).Build();
 
-var app = builder.Build();
+            // If development, migrate and seed the database.
+            // Migrate() also creates the database if it does not exist.
+            if (_Env.IsDevelopment() || _Env.IsStaging() || _Env.EnvironmentName == "StagingLocal")
+            {
+                using (var scope = host.Services.CreateScope())
+                {
+                    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+                    db.Database.Migrate();
 
-app.MapControllers();
+                    var seedDatabase = scope.ServiceProvider.GetRequiredService<ISeedDatabase>();
+                    seedDatabase.Seed();
 
-startup.Configure(app, app.Environment);
+                    // This auth code needs re-work.
+                    //var seedDbUsers = scope.ServiceProvider.GetRequiredService<ISeedAuth>();
+                    //seedDbUsers.Seed();
+                }
+            }
 
-app.Run();
+            host.Run();
+        }
+
+        /*
+        CreateDefaultBuilder automatically includes the appropriate appsettings.Environment.json.
+        Set the ASPNETCORE_ENVIRONMENT environment variable to "Development", "Production", etc.
+        For VsCode this can be set in Properties/launchSettings.json.
+        For Visual Studio, use: Project > Properties > Debug > Environment Variables.
+        */
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+            .ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>()
+                .ConfigureAppConfiguration((hostingContext, config) => 
+                {
+                    _Env = hostingContext.HostingEnvironment;
+                    BuildConfig.Build(config, _Env.EnvironmentName);
+                })
+                .ConfigureLogging(logging =>
+                {
+                    logging.ClearProviders();
+                    logging.SetMinimumLevel(LogLevel.Trace);
+                })
+                .UseNLog();  // NLog: setup NLog for Dependency injection
+            });
+    }
+}
